d518a1b1bd6dc7781549ebbe3ecacf9136ea4c2b
malo-gv <malogavard@gmail.com>
2024-03-05 16:20:21 +0100

    FLECHE VA TOUT DROITTTTTTTT

--- fonction de base apprentissage.py
+++ fonction de base apprentissage.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 from arrow import *
 from random import randint
+
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -17,18 +18,37 @@ character = pygame.transform.scale(character, (int(character.get_width() * 0.25)
 character = pygame.transform.flip(character, True, False)
 window.blit(character, (75, base_ground))
 
-keys = pygame.key.get_pressed()
+arrow_x = 172
+arrow_y = 467
+angle = -45
+arrow_visible = False
 
 running = True
+arrow_moving = False
+prev_time = pygame.time.get_ticks()
+keys = pygame.key.get_pressed()
 while running :
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-        if event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                show_arrow(window)
 
+        if event.type == KEYDOWN:
+            if event.key == K_DELETE:
+                show_arrow(window, arrow_x, arrow_y, angle)
+                arrow_visible = True
+
+            if event.key == K_SPACE:
+                arrow_moving = True
+    if arrow_moving:
+        current_time = pygame.time.get_ticks()
+        dt = (current_time - prev_time) / 1000.0
+        arrow_x = move_arrow_straight()
+        window.blit(background, (0, 20))
+        window.blit(character, (75, base_ground))
+        show_arrow(window, arrow_x, arrow_y, angle)
+        prev_time = current_time
 
     pygame.display.update()
     clock.tick(60)
+
 pygame.quit()
